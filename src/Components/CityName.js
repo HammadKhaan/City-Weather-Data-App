58193c81c11c487748efa37f23ee981990dc59bb
HammadKhaan <hammadkhan.1199@gmail.com>
2021-09-15 23:44:21 +0500

    App Completed

--- src/Components/CityName.js
+++ src/Components/CityName.js
@@ -5,19 +5,19 @@ import { Button } from "antd";
 
 const CityName = ({setFlip, setCity}) => {
   return (
-    <div className="site-card-wrapper">
+    <div className="site-card-wrapper" >
       <Row
         justify="center"
         style={{ padding: "20px 20px 20px 20px", alignItems: "center" }}
       >
-        <Col lg={10} md={10} sm={24} xs={24}>
+        <Col lg={24} md={20} sm={24} xs={24}>
           <Card
-            title="Enter City Name"
+            title="WEATHER DATA APP"
             bordered={true}
             style={{ border: "2px solid black" }}
 
           >
-            <Input placeholder="" style={{ border: "2px solid black" }} onChange={(e)=>setCity(e.target.value)}/>
+            <Input placeholder="" addonBefore="Enter City Name" style={{ border: "2px solid black" }} onChange={(e)=>setCity(e.target.value)}/>
             <Button type="primary" style={{ marginTop: "20px" }} onClick={()=>setFlip(true)}>
               Show Weather
             </Button>
--- src/Components/Data.js
+++ src/Components/Data.js
@@ -14,10 +14,24 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "antd";
 import axios from "axios";
+import Moment from "react-moment";
+import "moment-timezone";
+import "../Styles/Data.css";
 
 const Data = ({ setFlip, city }) => {
   const { Title } = Typography;
   const [weatherData, setWeatherData] = useState({});
+  const [weatherDay, setWeatherDay] = useState({});
+  const toUpperCaseFilter = (d) => {
+    return d.toUpperCase();
+  };
+
+  const weatherDayFunc = async (city) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=779e161f73821a08f47c9c2fcbb897e1`
+    );
+    return data;
+  };
 
   const weather = async (value) => {
     const { data } = await axios.get(
@@ -31,18 +45,27 @@ const Data = ({ setFlip, city }) => {
     setWeatherData(data);
   }, [city]);
 
+  useEffect(async () => {
+    const data2 = await weatherDayFunc(`${city}`);
+    setWeatherDay(data2);
+  }, [city]);
+
+  console.log(`weatherDay`, weatherDay);
+  
   return (
     <div>
       <div
         style={{
           border: "2px solid black",
           padding: "20px 20px 20px 20px",
-          margin: "20px 20px 0px 20px",
-          width: "maxContent",
+          margin: "0px 20px 0px 20px",
+          width: "80vw",
+          backgroundColor: "#2D4B5D",
+          
         }}
       >
         <Row style={{ textAlign: "center" }}>
-          <Col lg={8} md={8} sm={8} style={{ textAlign: "left" }}>
+          <Col lg={8} md={8} sm={8} xs={8}  style={{ textAlign: "left" }}>
             <Button
               type="primary"
               ghost
@@ -51,15 +74,16 @@ const Data = ({ setFlip, city }) => {
             >
               <FontAwesomeIcon
                 icon={faArrowLeft}
-                style={{ alignItems: "left", color: "black", fontSize: "20px" }}
+                style={{ alignItems: "left", color: "#F4FEFF", fontSize: "20px" }}
               />
             </Button>
           </Col>
-          <Col lg={8} md={8} sm={8}>
-            <Title level={4}>{`${weatherData?.name}, ${weatherData?.sys?.country}`}</Title>
+          <Col lg={8} md={8} sm={8} xs={8}>
+            <h2
+            >{(`${weatherData?.name}, ${weatherData?.sys?.country}`).toUpperCase()}</h2>
           </Col>
 
-          <Col lg={8} md={8} sm={8}></Col>
+          <Col lg={8} md={8} sm={8} xs={8}></Col>
         </Row>
         <Row style={{ paddingTop: "60px" }}>
           <Col lg={8} md={8} sm={8} xs={8}>
@@ -74,7 +98,7 @@ const Data = ({ setFlip, city }) => {
               icon={faTemperatureHigh}
               style={{ fontSize: "60px" }}
             />
-            <h1>{(weatherData?.main?.temp - 273.15).toFixed(2)}</h1>
+            <h1>{`${(weatherData?.main?.temp - 273.15).toFixed(2)}°C`}</h1>
           </Col>
           <Col lg={8} md={8} sm={8} xs={8}>
             <h2>{`Wind: ${weatherData?.wind?.speed} mph`}</h2>
@@ -84,56 +108,88 @@ const Data = ({ setFlip, city }) => {
         </Row>
         <Row justify="center" style={{ paddingTop: "60px" }}>
           <Col lg={4} md={8} sm={24} xs={24}>
-            <h3>SAT</h3>
+            <h3>
+              <Moment
+                format="D MMM YYYY"
+                withTitle
+                interval={0}
+                filter={toUpperCaseFilter}
+                date={weatherDay?.list?.[2]?.dt_txt}
+              />
+            </h3>
             <br />
             <FontAwesomeIcon
               icon={faCloudShowersHeavy}
-              style={{ fontSize: "40px" }}
+              style={{ fontSize: "40px", color:"#71ACF1" }}
             />
             <br />
             <br />
-            <h3>20°C</h3>
+            <h3>{`${(weatherDay?.list?.[2]?.main?.temp - 273.15).toFixed(1)}°C`}</h3>
             <br />
             <br />
           </Col>
           <Col lg={4} md={8} sm={24} xs={24}>
-            <h3>SUN</h3>
+            <h3><Moment
+                format="D MMM YYYY"
+                withTitle
+                interval={0}
+                filter={toUpperCaseFilter}
+                date={weatherDay?.list?.[10]?.dt_txt}
+              /></h3>
             <br />
-            <FontAwesomeIcon icon={faCloudSun} style={{ fontSize: "40px" }} />
+            <FontAwesomeIcon icon={faCloudSun} style={{ fontSize: "40px", color:"#71ACF1" }} />
             <br />
             <br />
-            <h3>20°C</h3>
+            <h3>{`${(weatherDay?.list?.[10]?.main?.temp - 273.15).toFixed(1)}°C`}</h3>
             <br />
             <br />
           </Col>
           <Col lg={4} md={8} sm={24} xs={24}>
-            <h3>MON</h3>
+            <h3><Moment
+                format="D MMM YYYY"
+                withTitle
+                interval={0}
+                filter={toUpperCaseFilter}
+                date={weatherDay?.list?.[18]?.dt_txt}
+              /></h3>
             <br />
             <FontAwesomeIcon
               icon={faCloudShowersHeavy}
-              style={{ fontSize: "40px" }}
+              style={{ fontSize: "40px", color:"#71ACF1" }}
             />
             <br />
             <br />
-            <h3>22°C</h3> <br />
+            <h3>{`${(weatherDay?.list?.[18]?.main?.temp - 273.15).toFixed(1)}°C`}</h3> <br />
             <br />
           </Col>
           <Col lg={4} md={8} sm={24} xs={24}>
-            <h3>TUE</h3>
+            <h3><Moment
+                format="D MMM YYYY"
+                withTitle
+                interval={0}
+                filter={toUpperCaseFilter}
+                date={weatherDay?.list?.[26]?.dt_txt}
+              /></h3>
             <br />
-            <FontAwesomeIcon icon={faCloudRain} style={{ fontSize: "40px" }} />
+            <FontAwesomeIcon icon={faCloudRain} style={{ fontSize: "40px", color:"#71ACF1" }} />
             <br />
             <br />
-            <h3>23°C</h3> <br />
+            <h3>{`${(weatherDay?.list?.[26]?.main?.temp - 273.15).toFixed(1)}°C`}</h3> <br />
             <br />
           </Col>
           <Col lg={4} md={8} sm={24} xs={24}>
-            <h3>WED</h3>
+            <h3><Moment
+                format="D MMM YYYY"
+                withTitle
+                interval={0}
+                filter={toUpperCaseFilter}
+                date={weatherDay?.list?.[34]?.dt_txt}
+              /></h3>
             <br />
-            <FontAwesomeIcon icon={faCloud} style={{ fontSize: "40px" }} />
+            <FontAwesomeIcon icon={faCloud} style={{ fontSize: "40px", color:"#71ACF1", }} />
             <br />
             <br />
-            <h3>18°C</h3> <br />
+            <h3>{`${(weatherDay?.list?.[34]?.main?.temp - 273.15).toFixed(1)}°C`}</h3> <br />
             <br />
           </Col>
         </Row>
